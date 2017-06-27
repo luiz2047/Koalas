3cad496f184baffb4624fdeb90d7a0ebb07a7626
luiz2047 <kochetkov2001@mail.ru>
2017-06-27 22:55:43 +0300

    circles

---
+++ js/main.js
@@ -0,0 +1,62 @@
+ $(document).ready(function () { // 6,32 5,38 2,34
+     $("#circle1").circliful({
+         animation: 1,
+         animationStep: 5,
+         foregroundBorderWidth: 15,
+         backgroundBorderWidth: 15,
+         percent: 100,
+         textSize: 24,
+         textStyle: 'font-size: 12px;',
+         textColor: '#666',
+         multiPercentage: 1,
+         percentages: [10, 20, 30]
+     });
+     $("#circle2").circliful({
+         animation: 1,
+         animationStep: 5,
+         foregroundBorderWidth: 15,
+         backgroundBorderWidth: 15,
+         percent: 100,
+         textSize: 24,
+         textStyle: 'font-size: 12px;',
+         textColor: '#666',
+         multiPercentage: 1,
+         percentages: [10, 20, 30]
+     });
+     $("#circle3").circliful({
+         animation: 1,
+         animationStep: 5,
+         foregroundBorderWidth: 15,
+         backgroundBorderWidth: 15,
+         percent: 100,
+         textSize: 24,
+         textStyle: 'font-size: 12px;',
+         textColor: '#666',
+         multiPercentage: 1,
+         percentages: [10, 20, 30]
+     });
+     $("#circle4").circliful({
+         animation: 1,
+         animationStep: 5,
+         foregroundBorderWidth: 15,
+         backgroundBorderWidth: 15,
+         percent: 100,
+         textSize: 24,
+         textStyle: 'font-size: 12px;',
+         textColor: '#666',
+         multiPercentage: 1,
+         percentages: [10, 20, 30]
+     });
+     $("#circle5").circliful({
+         animation: 1,
+         animationStep: 5,
+         foregroundBorderWidth: 15,
+         backgroundBorderWidth: 15,
+         percent: 100,
+         textSize: 24,
+         textStyle: 'font-size: 12px;',
+         textColor: '#666',
+         multiPercentage: 1,
+         percentages: [10, 20, 30]
+     });
+ });
