f286e36e36cd7fcd91719d2a52dcdef4490e6548
Mr.Tabrest <g.tabroid@gmail.com>
2017-06-29 03:48:56 +0300

    Optimization

--- js/main.js
+++ js/main.js
@@ -1,62 +1,74 @@
  $(document).ready(function () { // 6,32 5,38 2,34
-     $("#circle1").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
-     $("#circle2").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
-     $("#circle3").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
-     $("#circle4").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
-     $("#circle5").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
+ 	$("#circle1").circliful({
+ 		animation: 1,
+ 		animationStep: 7,
+ 		foregroundBorderWidth: 12,
+ 		backgroundBorderWidth: 17,
+ 		percent: 55,
+ 		textSize: 24,
+ 		textStyle: 'font-size: 120px;',
+ 		textColor: '#666',
+ 		multiPercentage: 1,
+ 		percentages: [10, 20, 30]
+ 	});
+ 	$("#circle2").circliful({
+ 		animation: 1,
+ 		animationStep: 7,
+ 		foregroundBorderWidth: 12,
+ 		backgroundBorderWidth: 17,
+ 		percent: 45,
+ 		textSize: 24,
+ 		textStyle: 'font-size: 12px;',
+ 		textColor: '#666',
+ 		multiPercentage: 1,
+ 		percentages: [10, 20, 30]
+ 	});
+ 	$("#circle3").circliful({
+ 		animation: 1,
+ 		animationStep: 7,
+ 		foregroundBorderWidth: 12,
+ 		backgroundBorderWidth: 17,
+ 		percent: 60,
+ 		textSize: 24,
+ 		textStyle: 'font-size: 12px;',
+ 		textColor: '#666',
+ 		multiPercentage: 1,
+ 		percentages: [10, 20, 30]
+ 	});
+ 	$("#circle4").circliful({
+ 		animation: 1,
+ 		animationStep: 7,
+ 		foregroundBorderWidth: 12,
+ 		backgroundBorderWidth: 17,
+ 		percent: 75,
+ 		textSize: 24,
+ 		textStyle: 'font-size: 12px;',
+ 		textColor: '#666',
+ 		multiPercentage: 1,
+ 		percentages: [10, 20, 30]
+ 	});
+ 	$("#circle5").circliful({
+ 		animation: 1,
+ 		animationStep: 7,
+ 		foregroundBorderWidth: 12,
+ 		backgroundBorderWidth: 17,
+ 		percent: 60,
+ 		textSize: 24,
+ 		textStyle: 'font-size: 12px;',
+ 		textColor: '#666',
+ 		multiPercentage: 1,
+ 		percentages: [10, 20, 30]
+ 	});
+ 	$("#circle6").circliful({
+ 		animation: 1,
+ 		animationStep: 7,
+ 		foregroundBorderWidth: 12,
+ 		backgroundBorderWidth: 17,
+ 		percent: 65,
+ 		textSize: 24,
+ 		textStyle: 'font-size: 12px;',
+ 		textColor: '#666',
+ 		multiPercentage: 1,
+ 		percentages: [10, 20, 30]
+ 	});
  });
