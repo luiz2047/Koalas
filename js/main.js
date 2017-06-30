da8de2d0b6fc7bf9b51b71ffdeccc5d0d1b19077
Mr.Tabrest <g.tabroid@gmail.com>
2017-06-30 11:11:47 +0300

    Come back

--- js/main.js
+++ js/main.js
@@ -1,69 +1,4 @@
  $(document).ready(function () { // 6,32 5,38 2,34
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-     $("#circle1").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
-     $("#circle2").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
-     $("#circle3").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
-     $("#circle4").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
-     $("#circle5").circliful({
-         animation: 1,
-         animationStep: 5,
-         foregroundBorderWidth: 15,
-         backgroundBorderWidth: 15,
-         percent: 100,
-         textSize: 24,
-         textStyle: 'font-size: 12px;',
-         textColor: '#666',
-         multiPercentage: 1,
-         percentages: [10, 20, 30]
-     });
-=======
->>>>>>> 5fdb3da836b8887579a33418d9eec0ed6a07e06d
  	$("#circle1").circliful({
  		animation: 1,
  		animationStep: 7,
@@ -136,8 +71,4 @@
  		multiPercentage: 1,
  		percentages: [10, 20, 30]
  	});
-<<<<<<< HEAD
-=======
->>>>>>> origin/Koalaz
->>>>>>> 5fdb3da836b8887579a33418d9eec0ed6a07e06d
  });
